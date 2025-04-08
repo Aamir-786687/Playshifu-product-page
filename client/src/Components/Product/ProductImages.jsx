4a4f018f09c5e67aaad6ca91f08f16432f2ddc2d
Aamir Ahmad <aamir557ahmad@gmail.com>
2025-04-08 12:52:32 +0530

    UI / Backend added

--- client/src/Components/Product/ProductImages.jsx
+++ client/src/Components/Product/ProductImages.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const ProductImages = () => {
   return (
-    <div>ProductImages</div>
-  )
-}
+    <div className="font-bold">
+      <h1>ProductImages</h1>
+    </div>
+  );
+};
 
-export default ProductImages
+export default ProductImages;
